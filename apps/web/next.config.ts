aeb0fea7791691c516bd692bb30eb3144f169a80
vinay bhat <bhatvinay74@gmail.com>
2025-09-03 16:45:48 +0530

    downgraded  next and react version

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('next').NextConfig} */
-import path from 'path'
- module.exports = {
-      reactStrictMode: true,
-      output: "standalone", // Required for standalone output
-      experimental: {
-        outputFileTracingRoot: path.join(__dirname, "../../"), // Adjust this path based on your app's location relative to the monorepo root
-      },
-    };
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+ 
+};
+
+export default nextConfig;
