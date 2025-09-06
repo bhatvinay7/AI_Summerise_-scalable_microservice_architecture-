718b9060b00f73973f9e66ceda148f65097df870
vinay bhat <bhatvinay74@gmail.com>
2025-09-06 18:41:49 +0530

    updated the workflow

--- apps/web/src/components/ui/ChatWindow.tsx
+++ apps/web/src/components/ui/ChatWindow.tsx
@@ -68,7 +68,7 @@ export default function ChatWindow() {
   useEffect(() => {
     try {
       if (!wsIntsance.current) {
-        const ws = new WebSocket("ws://localhost:8080");
+        const ws = new WebSocket(process.env.NEXT_PUBLIC_WEBSOCKET_SERVER!);
         wsIntsance.current = ws;
 
         wsIntsance.current.onopen = () => {
